83140c75239f59a1ae0465a849704a97cede6c87
GINISH SAHU <ginishkumardurg@gmail.com>
2025-08-20 17:02:41 +0530

    fix add function issue

--- test_app.py
+++ test_app.py
@@ -1,13 +1,6 @@
-import importlib.util
-import sys
-from pathlib import Path
-
-# dynamically load app.py as a module
-spec = importlib.util.spec_from_file_location("app", Path(__file__).parent / "app.py")
-app = importlib.util.module_from_spec(spec)
-sys.modules["app"] = app
-spec.loader.exec_module(app)
+import app
 
 def test_add():
     assert app.add(2, 3) == 5
+
 
